5a0ed7c0f85838a6eb85e9b4db7e355a78841176
pysocrates <ccarlsonfx@gmail.com>
2019-01-01 16:47:14 -0500

    PyCharm is not being very charming right now.

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 # Lets see where this takes us
 # Testing branch commits. Trying to use Git within an IDE. NuSkoolin it.
 # Why is PyCharm ignoring this file on commit... Let's take a look shall we?
+# Ive over estimated PyCharms "charm". Something is amiss.
+
 
 hellopy = "Hello Github!"
 print(hellopy)
