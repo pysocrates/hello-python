88198ba054a5903fa3a19859dd8aa9c3178e83b5
pysocrates <ccarlsonfx@gmail.com>
2019-01-01 16:44:59 -0500

    IDE git integration. Lovely tech.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # My first github commit.
 # Lets see where this takes us
 # Testing branch commits. Trying to use Git within an IDE. NuSkoolin it.
-# Why is PyCharm ignoring this file on commit... Let's take a look
+# Why is PyCharm ignoring this file on commit... Let's take a look shall we?
 
 hellopy = "Hello Github!"
 print(hellopy)
